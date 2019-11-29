b581e067f84ef81fbdd5f6801529f8101ceb5176
Guillaume Marcel <marcelguillaume54@gmail.com>
2019-11-29 15:10:19 +0100

    bruh

--- public/bundle.js
+++ public/bundle.js
@@ -103,9 +103,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
   !*** ./assets/styles/style.scss ***!
   \**********************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./assets/styles/style.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: File to import not found or unreadable: /Users/guillaumemarcel/www/vakars/assets/styles/connexion.scss.\\n        on line 4 of /Users/guillaumemarcel/www/vakars/assets/styles/style.scss\\n>> @import 'connexion.scss';\\n\\n   ^\\n\\n    at /Users/guillaumemarcel/www/vakars/node_modules/webpack/lib/NormalModule.js:316:20\\n    at /Users/guillaumemarcel/www/vakars/node_modules/loader-runner/lib/LoaderRunner.js:367:11\\n    at /Users/guillaumemarcel/www/vakars/node_modules/loader-runner/lib/LoaderRunner.js:233:18\\n    at context.callback (/Users/guillaumemarcel/www/vakars/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\\n    at Object.callback (/Users/guillaumemarcel/www/vakars/node_modules/sass-loader/dist/index.js:73:7)\\n    at Object.done [as callback] (/Users/guillaumemarcel/www/vakars/node_modules/neo-async/async.js:8067:18)\\n    at options.error (/Users/guillaumemarcel/www/vakars/node_modules/node-sass/lib/index.js:294:32)\");\n\n//# sourceURL=webpack:///./assets/styles/style.scss?");
 
 /***/ }),
 
